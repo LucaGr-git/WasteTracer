1d79ac6a998372fcf63192c36eaa6ebcd1549135
Joe Czerniecki <s4072773@student.rmit.edu.au>
2024-05-24 13:25:38 +1000

    preparing landing page javalin

--- src/main/java/app/PageIndex.java
+++ src/main/java/app/PageIndex.java
@@ -56,37 +56,8 @@ public class PageIndex implements Handler {
             </div>
         """;
 
-        // Add header content block
-        html = html + """
-            <div class='header'>
-                <h1>
-                    <img src='logo.png' class='top-image' alt='RMIT logo' height='75'>
-                    Homepage
-                </h1>
-            </div>
-        """;
-
         // Add Div for page Content
         html = html + "<div class='content'>";
-
-        // Add HTML for the page content
-        html = html + """
-            <p>Homepage content</p>
-            """;
-
-        // Get the ArrayList of Strings of all countries
-        ArrayList<String> countryNames = getAllCountries();
-
-        // Add HTML for the country list
-        html = html + "<h1>All Countries in the food loss database</h1>" + "<ul>";
-
-        // Finally we can print out all of the countries
-        for (String name : countryNames) {
-            html = html + "<li>" + name + "</li>";
-        }
-
-        // Finish the List HTML
-        html = html + "</ul>";
 
         // Close Content div
         html = html + "</div>";
@@ -98,68 +69,8 @@ public class PageIndex implements Handler {
             </div>
         """;
 
-        // Finish the HTML webpage
         html = html + "</body>" + "</html>";
 
-
-        // DO NOT MODIFY THIS
-        // Makes Javalin render the webpage
         context.html(html);
-    }
-
-
-    /**
-     * Get the names of the countries in the database.
-     */
-    public ArrayList<String> getAllCountries() {
-        // Create the ArrayList of String objects to return
-        ArrayList<String> countries = new ArrayList<String>();
-
-        // Setup the variable for the JDBC connection
-        Connection connection = null;
-
-        try {
-            // Connect to JDBC data base
-            connection = DriverManager.getConnection(JDBCConnection.DATABASE);
-
-            // Prepare a new SQL Query & Set a timeout
-            Statement statement = connection.createStatement();
-            statement.setQueryTimeout(30);
-
-            // The Query
-            String query = "SELECT * FROM country";
-            
-            // Get Result
-            ResultSet results = statement.executeQuery(query);
-
-            // Process all of the results
-            while (results.next()) {
-                String countryName  = results.getString("countryName");
-
-                // Add the country object to the array
-                countries.add(countryName);
-            }
-
-            // Close the statement because we are done with it
-            statement.close();
-        } catch (SQLException e) {
-            // If there is an error, lets just print the error
-            System.err.println(e.getMessage());
-            //e.printStackTrace();
-        } finally {
-            // Safety code to cleanup
-            try {
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                // connection close failed.
-                System.err.println(e.getMessage());
-                //e.printStackTrace();
-            }
-        }
-
-        // Finally we return all of the countries
-        return countries;
     }
 }
